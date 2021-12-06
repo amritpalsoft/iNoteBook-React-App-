c2269ce5afa3599997705521cae0db59c6c09536
amrit <amritpalsoftware@gmail.com>
2021-12-06 12:15:58 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const connectToMongo=require('./db')
+
+connectToMongo();
+
+const app=express();
+
+const port=3000;
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.listen(port,()=>{
+    console.log('listening');
+})
